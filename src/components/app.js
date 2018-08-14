2dea16f25f28a7a50c122b3652b63d403f68ad6c
IshantL <ishantlambhate@gmail.com>
2018-08-14 11:11:12 +0000

    Initialization

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Booklist from '../containers/books-list.js';
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      <Booklist/>
+      </div>
+    );
+  }
+}
